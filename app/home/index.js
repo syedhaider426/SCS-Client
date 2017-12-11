c57e70061317276771c505965c8320aca9da50c1
Paper-Hat <justinkristian@gmail.com>
2017-12-11 03:04:12 -0800

    .

--- app/home/index.js
+++ app/home/index.js
@@ -20,9 +20,10 @@
                 cd.user = user;
             });
         }
-    }
+    });
     module.exports.respond = function (socket_io) {
         socket_io.on('chat message', function (msg) {
             io.emit('chat message', msg);
-    });
+        });
+    };
 });
